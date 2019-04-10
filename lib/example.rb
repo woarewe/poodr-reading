b9465fd954b1fc8802ba4b04fcdd57cc8091290f
woarewe <rostislav.zhuravsky@gmail.com>
2019-04-10 14:55:08 +0300

    Test gpg

--- lib/example.rb
+++ lib/example.rb
@@ -1,2 +1,12 @@
-class Test; end
+# frozen_string_literal: true
+
+class Human
+  attr_accessor :age
+  attr_accessor :name
+
+  def initialize(age:, name:)
+    @name = name
+    @age = age
+  end
+end
 
