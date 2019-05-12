3cdee94ce6260f04fdfb5753b1240ff64a81ec46
woarewe <rostislav.zhuravsky@gmail.com>
2019-05-12 19:57:28 +0300

    Add final composition

---
+++ lib/chapter8/final_composition.rb
@@ -0,0 +1,66 @@
+# frozen_string_literal: true
+
+require 'forwardable'
+require 'ostruct'
+require 'pry-byebug'
+
+class Bicycle
+  attr_reader :size, :parts
+
+  def initialize(size: nil, parts: nil)
+    @size = size
+    @parts = parts
+  end
+
+  def spares
+    parts.spares
+  end
+end
+
+class Parts
+  extend Forwardable
+  include Enumerable
+
+  attr_reader :parts
+  def_delegators :parts, :size, :each
+
+  def initialize(*parts)
+    @parts = parts
+  end
+
+  def spares
+    select(&:needs_spare)
+  end
+end
+
+module PartsFactory
+  def self.build(config, parts_class: Parts)
+    parts_class.new(*config.map(&method(:create_part)))
+  end
+
+  def self.create_part(config)
+    OpenStruct.new(
+      name: config[0],
+      description: config[1],
+      needs_spare: config.fetch(2, true)
+    )
+  end
+end
+
+road_config = [
+  %w[chain 10-speed],
+  %w[tire_size 23],
+  %w[tape_color red]
+]
+
+mountain_config = [
+  %w[chain 10-seed],
+  %w[tire_size 2.1],
+  %w[front_shock Manitou] + Array(false),
+  %w[rear_shock Fox]
+]
+
+road_bike = Bicycle.new(size: 'L', parts: PartsFactory.build(road_config))
+p road_bike.spares
+mountain_bike = Bicycle.new(size: 'L', parts: PartsFactory.build(mountain_config))
+p mountain_bike.spares
